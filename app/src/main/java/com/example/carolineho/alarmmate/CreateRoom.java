b8157242e822a3f44741a4fce3d7809ac2aa1d53
carolineh101 <cho19@stanford.edu>
2016-06-26 01:16:58 -0700

    Added Firebase functionality.

--- app/src/main/java/com/example/carolineho/alarmmate/CreateRoom.java
+++ app/src/main/java/com/example/carolineho/alarmmate/CreateRoom.java
@@ -1,13 +1,80 @@
 package com.example.carolineho.alarmmate;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class CreateRoom extends AppCompatActivity {
+
+    private Button mJoin;
+    private Button mCreate;
+    private EditText mNewRoom;
+    private EditText mRoommateEmail;
+    private EditText mRoomName;
+    private DatabaseReference mDatabase;
+    private String name;
+    private String email;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_room);
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mJoin = (Button) findViewById(R.id.Join);
+        mCreate = (Button) findViewById(R.id.Create);
+        mNewRoom = (EditText) findViewById(R.id.NewRoom);
+        mRoommateEmail = (EditText) findViewById(R.id.RoommateEmail);
+        mRoomName = (EditText) findViewById(R.id.RoomName);
+        name = "";
+        email = "";
+
+        Intent intent = getIntent();
+        Bundle bd = intent.getExtras();
+        if(bd != null)
+        {
+            name = (String) bd.get("name");
+            email = (String) bd.get("email");
+        }
+
+        View.OnClickListener newRoomListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                mDatabase.child("rooms").child(mNewRoom.getText().toString()).setValue(mNewRoom.getText().toString());
+                mDatabase.child("rooms").child((mNewRoom.getText().toString())).child("user1").child("name").setValue(name);
+                mDatabase.child("rooms").child((mNewRoom.getText().toString())).child("user1").child("email").setValue(email);
+                mDatabase.child("rooms").child((mNewRoom.getText().toString())).child("user2").child("email").setValue(mRoommateEmail.getText().toString());
+
+                Intent intent = new Intent(CreateRoom.this, EnterTimes.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        };
+
+        mCreate.setOnClickListener(newRoomListener);
+
+        View.OnClickListener joinRoomListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //if (mDatabase.child("rooms").child((mRoomName.getText().toString())).child("user2").child("email").getValue().equals(email)) {
+                    mDatabase.child("rooms").child((mRoomName.getText().toString())).child("user2").child("name").setValue(name);
+                //}
+
+                Intent intent = new Intent(CreateRoom.this, EnterTimes.class);
+                intent.putExtra("name", name);
+                startActivity(intent);
+            }
+        };
+
+        mJoin.setOnClickListener(joinRoomListener);
     }
 }
--- app/src/main/java/com/example/carolineho/alarmmate/SignIn.java
+++ app/src/main/java/com/example/carolineho/alarmmate/SignIn.java
@@ -7,63 +7,42 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import com.firebase.client.AuthData;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.Map;
 
 public class SignIn extends AppCompatActivity {
 
-    private Firebase myFirebaseRef;
     private Button mSignUp;
+    private EditText mSignUpName;
     private EditText mSignUpEmail;
     private EditText mSignUpPassword;
-    //private Boolean mOpenedBefore = false;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
 
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
         mSignUp = (Button) findViewById(R.id.SignUp);
+        mSignUpName = (EditText) findViewById(R.id.SignUpName);
         mSignUpEmail = (EditText) findViewById(R.id.SignUpEmail);
         mSignUpPassword = (EditText) findViewById(R.id.SignUpPassword);
 
-        Firebase.setAndroidContext(this);
-        myFirebaseRef = new Firebase("https://alarmmate-fa8d9.firebaseio.com/");
 
         View.OnClickListener signUpListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //mOpenedBefore = true;
-
-                myFirebaseRef.createUser(mSignUpEmail.getText().toString(), mSignUpPassword.getText().toString(), new Firebase.ValueResultHandler<Map<String, Object>>() {
-                    @Override
-                    public void onSuccess(Map<String, Object> result) {
-                        System.out.println("Successfully created user account with uid: " + result.get("uid"));
-                    }
-                    @Override
-                    public void onError(FirebaseError firebaseError) {
-                        // there was an error
-                    }
-                });
-
-                myFirebaseRef.setValue("test");
-
-                myFirebaseRef.authWithPassword(mSignUpEmail.getText().toString(), mSignUpPassword.getText().toString(), new Firebase.AuthResultHandler() {
-                    @Override
-                    public void onAuthenticated(AuthData authData) {
-                        System.out.println("User ID: " + authData.getUid() + ", Provider: " + authData.getProvider());
-                    }
-                    @Override
-                    public void onAuthenticationError(FirebaseError firebaseError) {
-                        // there was an error
-                    }
-                });
+                mDatabase.child("users").child(mSignUpName.getText().toString()).child("name").setValue(mSignUpName.getText().toString());
+                mDatabase.child("users").child(mSignUpName.getText().toString()).child("email").setValue(mSignUpEmail.getText().toString());
+                mDatabase.child("users").child(mSignUpName.getText().toString()).child("password").setValue(mSignUpPassword.getText().toString());
 
                 Intent intent = new Intent(SignIn.this, CreateRoom.class);
+                intent.putExtra("name", mSignUpName.getText().toString());
+                intent.putExtra("email", mSignUpEmail.getText().toString());
                 startActivity(intent);
             }
         };
